efaa43bd97bf3d0f2ee616a3d0a33d931e9f816a
marialuiza1324 <marialuiza.nerypinto@yahoo.com.br>
2024-10-22 20:47:03 -0300

    table veiculos e reservas

---
+++ minicurso_laravel/database/migrations/2024_10_22_233154_create_veiculos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInter('id_categoria');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('ano_fabricacao');
+            $table->float('valor');
+            $table->string('placa')->unique();
+            $table->enum('status', ['disponivel', 'alugado', 'manutencao'])->default('disponivel');
+            $table->timestamps();
+    
+            $table->foreign('id_categoria')->references('id')->on('categorias'); //nome da coluna da sua tabela, nome da coluna da foreign key e o noma da tabela
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('veiculos');
+    }
+};
