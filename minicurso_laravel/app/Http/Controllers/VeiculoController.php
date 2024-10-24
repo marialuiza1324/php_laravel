550fb976715e53d17b7e1efa54e7c11c4b0ee834
marialuiza1324 <marialuiza.nerypinto@yahoo.com.br>
2024-10-23 21:04:12 -0300

    veiculo controller

--- minicurso_laravel/app/Http/Controllers/VeiculoController.php
+++ minicurso_laravel/app/Http/Controllers/VeiculoController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+Use App\Models\Veiculo;
 
 class VeiculoController extends Controller
 {
-    //
+    public function index(){
+        $veiculos = Veiculo::with('categorias')->get(); // trazer junto com a resposta os dados da categoria respectiva
+        return response()->json($veiculos);
+    }
+
+    public function show($id){
+        $veiculo = Veiculo::with('categorias')->find($id);
+
+        if(!$veiculo){
+            return response()->json(["message" => "Veículo não encontrado"], 400); // status 400 -> informar erro
+        }
+
+        return response()->json($veiculo);
+    }
+
+    public function store(StoreUpdateVeiculoRequest $request){
+        $veiculo = Veiculo::create($request->all());
+
+        $veiculo->save();
+
+        if($request->hasFile('img') && $request->file('img')->isValid()){
+            $img = $request->file('img');
+            $imgName = Str::slug($request->modelo) . '.' - $img->getClientOriginalExtension(); // formatacao de texto (tira acento e pontuação)
+
+            $imgPath = $img->storeAs("veiculos/{$veiculo->id}", $imgName, "public"); // cria a pasta em public
+            $veiculo->img = "storage/veiculos/{$veiculo->id}/{$imgName}";
+            $veiculo->save;
+        }
+
+        return response()->json($veiculo, 201);
+    }
 }
